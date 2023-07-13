47a879b7e0881b47fc9b04d3d6cac87d8152e7ab
Samuel Alison <samuelalison13@gmail.com>
2023-07-13 22:18:18 +0100

    changes from the new lappy

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -45,7 +45,7 @@ const AboutSection = () => {
             <br />
             <p>
               I have a wide range of hobbies and passions that keep me busy.
-              From reading, playing sports, traveling, to making making music,
+              From reading, playing sports, traveling, to playing the piano,
               I am always seeking new experiences and love to keep myself
               engaged and learning new things.
             </p>
