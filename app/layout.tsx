8d93b4c0933fe733f75ddf7d4a42a233f00c9025
Samuel Alison <samuelalison13@gmail.com>
2023-12-09 18:05:20 +0100

    tried not to return jsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,50 @@
 "use client"
 import "../styles/globals.css"
-import Navbar from "@/components/Navbar"
-import Footer from "@/components/Footer"
-import { ThemeProvider } from "next-themes"
+// import Navbar from "@/components/Navbar"
+// import Footer from "@/components/Footer"
+// import { ThemeProvider } from "next-themes"
 
-export type ThemeProviderProps = {
-  enableSystem?: boolean;
-  attribute?: string;
-  // Other props specific to the ThemeProvider component
-};
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode
+// }) {
+//   return (
+//     <html lang="en">
+//       {/*
+//         <head /> will contain the components returned by the nearest parent
+//         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+//       */}
+//       {/* <head /> */}
+//       <body className="dark:bg-stone-900">
+//         <ThemeProvider enableSystem={true} attribute="class">
+//           <Navbar />
+//           {children}
+//           <Footer />
+//         </ThemeProvider>
+//       </body>
+//     </html>
+//   )
+// }
+
+
+import React from 'react';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import { ThemeProvider } from 'next-themes';
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      {/* <head /> */}
-      <body className="dark:bg-stone-900">
-        <ThemeProvider enableSystem={true} attribute="class">z
-          <Navbar />
-          {children}
-          <Footer />
-        </ThemeProvider>
-      </body>
-    </html>
-  )
+  children: React.ReactNode;
+}): React.ReactElement {
+  return React.createElement('html', { lang: 'en' },
+    React.createElement('body', { className: 'dark:bg-stone-900' },
+      React.createElement(ThemeProvider, { enableSystem: true, attribute: 'class' },
+        React.createElement(Navbar),
+        children,
+        React.createElement(Footer)
+      )
+    )
+  );
 }
