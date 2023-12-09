d8fadce0b8436a6e81a5d64cc15993b3c769c586
Samuel Alison <samuelalison13@gmail.com>
2023-12-09 09:48:15 +0100

    added devdepndency

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -37,7 +37,9 @@ export default function Navbar() {
       <div className="justify-between md:items-center md:flex">
         <div>
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
-            <Link to="home">
+            <Link to="home"
+            as="any"
+            >
               <div className="container flex items-center space-x-2">
                 <h2 className="text-2xl font-bold">Samuel Alison</h2>
               </div>
